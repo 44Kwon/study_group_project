0bf8849d802c1da7526e608c6fea262219c091d8
44Kwon <kwh3217@gmail.com>
2025-05-06 19:05:23 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/group_platform/GroupPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

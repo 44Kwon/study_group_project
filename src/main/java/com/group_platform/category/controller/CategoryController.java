17ed7eeadfe51d5b44f87e7bfdee81b930e35fe4
44Kwon <kwh3217@gmail.com>
2025-05-29 00:21:25 +0900

    스터디그룹 등록 시 카테고리 선택을 위한 api

--- src/main/java/com/group_platform/category/controller/CategoryController.java
+++ src/main/java/com/group_platform/category/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.group_platform.category.controller;
 
+import com.group_platform.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CategoryController {
 
-
+    private final CategoryService categoryService;
     @GetMapping
     //스터디그룹 등록 시 카테고리 선택을 위해
     public ResponseEntity<?> getCategory() {
-        return null;
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(categoryService.getCategoryTypeList());
     }
 }

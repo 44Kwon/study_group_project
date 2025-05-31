1c145ff3723e973939b15bd75c072c908c1459d9
44Kwon <kwh3217@gmail.com>
2025-05-31 16:57:48 +0900

    게시글 관련 api 완료(목록조회, 검색, 좋아요,찜 등)

--- src/main/java/com/group_platform/post/controller/PostController.java
+++ src/main/java/com/group_platform/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.group_platform.post.controller;
 
-import com.group_platform.post.dto.PostDto;
+import com.group_platform.post.dto.*;
+import com.group_platform.post.entity.PostType;
 import com.group_platform.post.service.PostService;
 import com.group_platform.response.ResponseDto;
 import com.group_platform.security.dto.CustomUserDetails;
@@ -8,12 +9,17 @@ import com.group_platform.util.UriComponent;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -48,13 +54,22 @@ public class PostController {
                                         @RequestBody @Valid PostDto.UpdateRequest updateRequest,
                                         @PathVariable("post-id") @Positive Long postId) {
         updateRequest.setId(postId);
-        PostDto.Response response = postService.updatePost(userDetails.getId(), updateRequest);
+        PostResponseDto response = postService.updatePost(userDetails.getId(), updateRequest);
         return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(response));
     }
 
     @GetMapping("/posts/{post-id}")
-    public ResponseEntity<?> getPost(@PathVariable("post-id") @Positive Long postId) {
-        PostDto.Response post = postService.getPost(postId);
+    public ResponseEntity<?> getCommonPost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                           @PathVariable("post-id") @Positive Long postId) {
+        PostResponseDto post = postService.getCommonPost(userDetails.getId(),postId);
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(post));
+    }
+
+    @GetMapping("/study-group/{study-group-id}/posts/{post-id}")
+    public ResponseEntity<?> getGroupPost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                          @PathVariable("study-group-id") @Positive Long groupId,
+                                          @PathVariable("post-id") @Positive Long postId) {
+        PostResponseDto post = postService.getGroupPost(userDetails.getId(), groupId, postId);
         return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(post));
     }
 
@@ -65,41 +80,96 @@ public class PostController {
         return ResponseEntity.noContent().build();
     }
 
-//    @GetMapping("/posts")
-//    //공통 게시글 조회
-//    //(검색/필터 지원 가능)
-//    public ResponseEntity<?> getAllCommonPosts() {
-//        // (공통 게시글 상단고정 불가 로직 구현 필요)
-//    }
-//
-//    @GetMapping("/posts/search")
-//    public ResponseEntity<?> searchCommonPost(@RequestParam String query) {
-//
-//    }
-//
-//    //그룹 게시글 목록
-//    //(검색/필터 지원 가능)
-//    @GetMapping("/study-group/{study-group-id}/posts")
-//    public ResponseEntity<?> getAllGroupPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
-//                                              @PathVariable("study-group-id") @Positive Long studyGroupId) {
-//
-//    }
-//
-//    @GetMapping("/study-group/{study-group-id}/posts/search")
-//    public ResponseEntity<?> getAllGroupPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
-//                                              @PathVariable("study-group-id") @Positive Long studyGroupId,
-//                                              @RequestParam String query) {
-//
-//    }
-    // 공통 게시판에서 당일 상위 10개에 대한 목록(추천개념)
+    //최신순, 댓글 갯수순, 좋아요 순, 조회순
+    //게시글 타입별 필터링
+    @GetMapping("/posts")
+    public ResponseEntity<?> getPagedCommonPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                 @RequestParam(required = false) PostType postType,
+                                                 @RequestParam(defaultValue = "LATEST") PostSortType sort,
+                                                 @PageableDefault(size = 10) Pageable pageable) {
+        // (공통 게시글 상단고정 불가)
+        Page<PostResponseListDto> commonPosts = postService.getCommonPosts(userDetails.getId(), postType, sort, pageable);
+        List<PostResponseListDto> content = commonPosts.getContent();
+        return new ResponseEntity<>(new ResponseDto.MultipleResponseDto<>(content,commonPosts), HttpStatus.OK);
+    }
 
+    @GetMapping("/study-group/{study-group-id}/posts")
+    public ResponseEntity<?> getPagedGroupPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                @PathVariable("study-group-id") @Positive Long groupId,
+                                                @RequestParam(required = false) PostType postType,
+                                                @RequestParam(defaultValue = "LATEST") PostSortType sort,
+                                                @PageableDefault(size = 10) Pageable pageable) {
+        //새로운 ResponseDto (고정글 + 게시글목록)만들어서 내보내는중
+        PostListResponse groupPosts = postService.getGroupPosts(userDetails.getId(), groupId, postType, sort, pageable);
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(groupPosts));
+    }
 
-//    POST /posts/{id}/likes : 게시글 좋아요
-//
-//    DELETE /posts/{id}/likes : 게시글 좋아요 취소
-//
-//    찜/ 찜취소
-    //공통 게시글에 대해서 상단고정은 불가하고, 상단에 당일에 인기글 목록(10개)을 보여준다
+    @GetMapping("/posts/search")
+    public ResponseEntity<?> getPagedSearchCommonPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                       @RequestParam("q") String query,
+                                                       @PageableDefault(size = 10) Pageable pageable) {
+        Page<PostResponseListDto> postResponseListDtos = postService.searchCommonPosts(userDetails.getId(), query, pageable);
+        List<PostResponseListDto> content = postResponseListDtos.getContent();
+        return new ResponseEntity<>(new ResponseDto.MultipleResponseDto<>(content,postResponseListDtos), HttpStatus.OK);
+    }
 
-    //상단고정, 취소(그룹 내 게시판에서만)
+    @GetMapping("/study-group/{study-group-id}/posts/search")
+    public ResponseEntity<?> getPagedSearchGroupPosts(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                      @PathVariable("study-group-id") @Positive Long groupId,
+                                                      @RequestParam("q") String query,
+                                                      @PageableDefault(size = 10) Pageable pageable) {
+        PostListResponse postListResponse = postService.searchGroupPosts(userDetails.getId(), groupId, query, pageable);
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(postListResponse));
+    }
+
+    @PatchMapping("/study-group/{study-group-id}/posts/{post-id}/fixed")
+    //상단고정(리더만가능)
+    public ResponseEntity<?> pinPost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                     @PathVariable("study-group-id") @Positive Long groupId,
+                                     @PathVariable("post-id") @Positive Long postId) {
+        PostDto.UpdatePinnedResponse updatePinnedResponse = postService.updatePinned(userDetails.getId(), groupId, postId);
+        return new ResponseEntity<>(new ResponseDto.SingleResponseDto<>(updatePinnedResponse), HttpStatus.OK);
+    }
+
+    @PatchMapping("/study-group/{study-group-id}/posts/{post-id}/unfixed")
+    //상단고정취소(리더만가능)
+    public ResponseEntity<?> unpinPost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                       @PathVariable("study-group-id") @Positive Long groupId,
+                                       @PathVariable("post-id") @Positive Long postId) {
+        PostDto.UpdatePinnedResponse updatePinnedResponse = postService.updateUnPinned(userDetails.getId(), groupId, postId);
+        return new ResponseEntity<>(new ResponseDto.SingleResponseDto<>(updatePinnedResponse), HttpStatus.OK);
+    }
+
+    //좋아요
+    @PostMapping("/posts/{post-id}/likes")
+    public ResponseEntity<?> likePost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                      @PathVariable("post-id") @Positive Long postId) {
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(postService.likePost(userDetails.getId(), postId)));
+    }
+
+    //좋아요취소
+    @DeleteMapping("/posts/{post-id}/likes")
+    public ResponseEntity<?> cancelLikePost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                            @PathVariable("post-id") @Positive Long postId) {
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(postService.cancelLikePost(userDetails.getId(), postId)));
+    }
+
+    //찜
+    @PostMapping("/posts/{post-id}/favorites")
+    public ResponseEntity<?> favoritePost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                          @PathVariable("post-id") @Positive Long postId) {
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(postService.favoritePost(userDetails.getId(), postId)));
+    }
+
+    //찜 취소
+    @DeleteMapping("/posts/{post-id}/favorites")
+    public ResponseEntity<?> cancelFavoritePost(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                          @PathVariable("post-id") @Positive Long postId) {
+        return ResponseEntity.ok(new ResponseDto.SingleResponseDto<>(postService.cancelFavoritePost(userDetails.getId(), postId)));
+    }
+//    @GetMapping("/posts/top")
+//    // 공통 게시판에서 당일 상위 10개에 대한 목록(추천개념)
+//    public ResponseEntity<?> getTopCommonPosts() {
+//        return null;
+//    }
 }

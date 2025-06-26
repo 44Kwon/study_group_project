307f067b78155a66ea053433d6016e2c9ad100be
44Kwon <kwh3217@gmail.com>
2025-06-26 14:17:50 +0900

    스케줄링 활성화

--- src/main/java/com/group_platform/GroupPlatformApplication.java
+++ src/main/java/com/group_platform/GroupPlatformApplication.java
@@ -3,8 +3,10 @@ package com.group_platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing //Auditing 기능을 쓰기 위해 필수
+@EnableScheduling   //스케줄링 기능 활성화
 @SpringBootApplication
 public class GroupPlatformApplication {
 

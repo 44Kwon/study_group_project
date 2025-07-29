95a917f058eca729fc28b6935c68c1dc70ac636c
44Kwon <kwh3217@gmail.com>
2025-07-29 16:59:27 +0900

    비동기 작업을 위한 쓰레드 풀 설정과 비동기 예외 처리 핸들

--- src/main/java/com/group_platform/config/AsyncConfig.java
+++ src/main/java/com/group_platform/config/AsyncConfig.java
@@ -1,9 +1,39 @@
 package com.group_platform.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+@Slf4j
+public class AsyncConfig implements AsyncConfigurer {
+
+    // 비동기 작업을 위한 스레드 풀 설정
+    // 향후 여러 Executor를 만들어 놓고 목적과 성능에 맞게 선택해서 사용한다.
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // 기본 스레드 수
+        executor.setMaxPoolSize(10); // 최대 스레드 수
+        executor.setQueueCapacity(100); // 큐 용량
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    // AsyncConfigurer 인터페이스를 구현하여 비동기 예외 처리 핸들
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            // 비동기 예외 처리 로직
+            log.error("비동기 예외 발생 - 메서드: {}, 메시지: {}", method.getName(), ex.getMessage(), ex);
+        };
+    }
 }

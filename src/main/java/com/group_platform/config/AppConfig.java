c8b91ea6664f9b7154fc32bc123d3e0892e1e191
44Kwon <kwh3217@gmail.com>
2025-07-29 16:59:48 +0900

    스케줄러를 위한 쓰레드 설정

--- src/main/java/com/group_platform/config/AppConfig.java
+++ src/main/java/com/group_platform/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.group_platform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +11,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    // 스케줄러를 위한 ThreadPoolTaskScheduler 빈 설정
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("Scheduler-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
